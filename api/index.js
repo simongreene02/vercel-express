c9108e3bb8e44db45080d24eee23c69b4c9098cb
Simon Green <simongreen02@gmail.com>
2024-05-23 04:01:46 -0500

    Possible fix

--- api/index.js
+++ api/index.js
@@ -6,4 +6,4 @@ app.get("/", (req, res) => res.send("Express on Vercel"));
 
 app.listen(3000, () => console.log("Server ready on port 3000."));
 
-export default app;
+export default { path: '/api', handler: app };
